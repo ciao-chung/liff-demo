a7dc9ba7183bdc013ad115b7e6ce9105b104415d
Ciao Chung <ciao0958@gmail.com>
2022-06-07 17:55:43 +0800

    publish

--- src/route.ts
+++ src/route.ts
@@ -6,10 +6,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 Vue.use(Router)
-
+const isProductionMode = process.env.NODE_ENV == 'production'
 export function createRouter() {
   return new Router({
     mode: 'history',
+    base: isProductionMode ? '/liff-demo/' : '/',
     routes: [
       {
         path: `/`,
