001c0916173723aef0419de3fd868148353463e3
Ciao Chung <ciao0958@gmail.com>
2022-06-08 21:58:36 +0800

    publish

--- pages/home.vue
+++ pages/home.vue
@@ -93,11 +93,13 @@ export default {
               type: 'buttons',
               // thumbnailImageUrl: 'https://images.unsplash.com/photo-1528994618239-4d83bbdb7a0f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxleHBsb3JlLWZlZWR8MTl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
               text: '請點此加入餐廳',
-              actions: {
-                type: 'uri',
-                label: '請點此加入餐廳',
-                uri: `https://www.google.com?code=${this.otp}`,
-              },
+              actions: [
+                {
+                  type: 'uri',
+                  label: '請點此加入餐廳',
+                  uri: `https://www.google.com?code=${this.otp}`,
+                }
+              ],
             },
           },
         ])
